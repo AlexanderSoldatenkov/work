654902937735259309cef42025bac494f4980fc7
Alexander <xspikyx@gmail.com>
2022-04-02 20:19:21 +0300

    .

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+    const tabs = require('./modules/tabs'),
+        modal = require('./modules/modal'),
+        calc = require('./modules/calc'),
+        cards = require('./modules/cards'),
+        forms = require('./modules/forms'),
+        slider = require('./modules/slider'),
+        timer = require('./modules/timer');
+
+    tabs();
+    modal();
+    calc();
+    cards();
+    forms();
+    slider();
+    timer();
+
+});
