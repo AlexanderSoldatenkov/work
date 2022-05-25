810003c980e7d7aef4ce7e7e7ebb248239278dca
Alexander <xspikyx@gmail.com>
2022-05-25 10:36:21 +0300

    .

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,11 @@
-import tabs from './modules/tabs';
-import modal from './modules/modal';
-import calc from './modules/calc';
-import cards from './modules/cards';
-import forms from './modules/forms';
-import slider from './modules/slider';
-import timer from './modules/timer';
-import { openModal } from './modules/modal';
+// import tabs from './modules/tabs';
+// import modal from './modules/modal';
+// import calc from './modules/calc';
+// import cards from './modules/cards';
+// import forms from './modules/forms';
+// import slider from './modules/slider';
+// import timer from './modules/timer';
+// import { openModal } from './modules/modal';
 
 import '../../node_modules/focus-visible/dist/focus-visible';
 
@@ -16,23 +16,23 @@ import '../index.html';
 
 
 
-window.addEventListener('DOMContentLoaded', function () {
-  const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 300000);
-  tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
-  modal('[data-modal]', '.modal', modalTimerId);
-  calc();
-  cards();
-  forms('form', modalTimerId);
-  slider({
-      container: '.offer__slider',
-      nextArrow: '.offer__slider-next',
-      previousArrow: '.offer__slider-prev',
-      totalCounter: '#total',
-      currentCounter: '#current',
-      wrapper: '.offer__slider-wrapper',
-      field: '.offer__slider-inner',
-      slide: '.offer__slide'
-  });
-  timer('.timer', '2020-06-11');
+// window.addEventListener('DOMContentLoaded', function () {
+//   const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 300000);
+//   tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+//   modal('[data-modal]', '.modal', modalTimerId);
+//   calc();
+//   cards();
+//   forms('form', modalTimerId);
+//   slider({
+//       container: '.offer__slider',
+//       nextArrow: '.offer__slider-next',
+//       previousArrow: '.offer__slider-prev',
+//       totalCounter: '#total',
+//       currentCounter: '#current',
+//       wrapper: '.offer__slider-wrapper',
+//       field: '.offer__slider-inner',
+//       slide: '.offer__slide'
+//   });
+//   timer('.timer', '2020-06-11');
 
-});
+// });
